4ccc6d2a9167443d7088ef9919a2d70a28625b23
Ana Beatriz <biamaiaribeiro@gmail.com>
2024-01-29 10:38:57 -0300

    Deletar dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -17,18 +18,16 @@ public class Program {
 			
 			conn = DB.getConnection();
 			
-			st = conn.prepareStatement("UPDATE seller SET BaseSalary = BaseSalary + ? " + "WHERE (DepartmentId = ?)"); // Comando SQL
+			st = conn.prepareStatement("DELETE FROM department " + "WHERE " + "Id = ?"); // Comando SQL
 			
 			// Atribuindo os valores
-			st.setDouble(1, 200.0);
-			st.setInt(2, 2);
-			
+			st.setInt(1, 2);
 			int rowsAffected = st.executeUpdate();
 			
 			System.out.println("Done! Rows affected: " + rowsAffected);
 			
 		} catch (SQLException e) {
-			e.printStackTrace();
+			throw new DbIntegrityException(e.getMessage());
 		}
 		
 		DB.closeStatement(st);
